6c1166810a324e21c61ea71db78995d02c1eb8f1
MyungKi You <ymkmoon@naver.com>
2025-08-08 10:35:38 +0900

    토큰 예외 메세지 변경

--- src/main/java/com/example/template/config/JwtConfig.java
+++ src/main/java/com/example/template/config/JwtConfig.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
+import com.example.template.error.ErrorCode;
+import com.example.template.exception.BusinessException;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -28,13 +31,13 @@ public class JwtConfig {
         return tokens.stream()
                 .filter(token -> token.getGroup().equalsIgnoreCase(group))
                 .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException("Invalid token type: " + group));
+                .orElseThrow(() -> new BusinessException(ErrorCode.TOKEN_IS_NOT_AUTHORIZED));
     }
 
     @Getter
     @Setter
     public static class TokenConfig {
-        private String group;     // CommonConstants > ACCESS_TOKEN, LoginId
+        private String group;     // CommonConstants > ACCESS_TOKEN, REFRESH_TOKEN
         private String secretKey;
         private long validity;    // milliseconds
     }
