a6968ea654a3b5ccc6f2a5db30b3882e51c4400e
ymkmoon <ymkmoon@naver.com>
2025-08-14 17:44:20 +0900

    chore: 로컬 환경과 개발 환경 프로파일, DB 분리

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,10 +1,7 @@
--- write 계정 생성
-CREATE USER IF NOT EXISTS WRITE_USER PASSWORD 'write_password';
-GRANT ALL ON SCHEMA PUBLIC TO WRITE_USER;
-
--- read 계정 생성
-CREATE USER IF NOT EXISTS READ_USER PASSWORD 'read_password';
-GRANT SELECT ON SCHEMA PUBLIC TO READ_USER;
+CREATE USER IF NOT EXISTS write_user PASSWORD 'write_password';
+CREATE USER IF NOT EXISTS read_user PASSWORD 'read_password';
+GRANT ALL ON SCHEMA PUBLIC TO write_user;
+GRANT SELECT ON SCHEMA PUBLIC TO read_user;
 
 INSERT INTO authority_level (level_code, description, created_by, updated_by) VALUES ('SUPER_ADMIN', '최고 관리자', 'SYSTEM', 'SYSTEM');
 INSERT INTO authority_level (level_code, description, created_by, updated_by) VALUES ('MID_ADMIN', '중간 관리자', 'SYSTEM', 'SYSTEM');
